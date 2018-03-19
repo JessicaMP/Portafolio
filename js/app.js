d41a2495b55fe0a67e5dbffd1986dd3f9bcf6f7a
JessicaMP <jessica95.mp@gmail.com>
2018-03-19 04:36:17 -0500

    Vista en desktop completo

--- js/app.js
+++ js/app.js
@@ -1,6 +1,35 @@
-//Materialize
-
-$(document).ready(function(){
-    $(".button-collapse").sideNav();
-    $('.tooltipped').tooltip({delay: 50});
+$(document).ready(function ($) {
+  $("section.tab-pane").addClass('d-none');
+  $("section.tab-pane.show.active").removeClass('d-none');
+  $("li > a").click(function () {
+    console.log(this);
+    $("section.tab-pane").addClass('d-none');
+    $("section.tab-pane.show.active").removeClass('d-none');
   });
+});
+
+$(document).ready(function() {
+  let spans = document.querySelectorAll('.word span');
+  spans.forEach((span, idx) => {
+    span.addEventListener('click', (event) => {
+      event.target.classList.add('active');
+    });
+    span.addEventListener('animationend', (event) => {
+      event.target.classList.remove('active');
+    });
+
+    setTimeout(() => {
+      span.classList.add('active');
+    }, 750 * (idx + 1));
+  });
+
+  $('.skillbar').each(function() {
+    $(this).find('.skillbar-bar').animate({
+      width: $(this).attr('data-percent')
+    }, 20000);
+  });
+
+  $('.hover').mouseleave(function() {
+    $(this).removeClass('hover');
+  });
+});
