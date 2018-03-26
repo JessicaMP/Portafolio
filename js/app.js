017a6c6b1a5c34a627811ef9fa14941106ee6644
JessicaMP <jessica95.mp@gmail.com>
2018-03-26 09:42:29 -0500

    Vista mobile completa

--- js/app.js
+++ js/app.js
@@ -6,30 +6,6 @@ $(document).ready(function ($) {
     $("section.tab-pane").addClass('d-none');
     $("section.tab-pane.show.active").removeClass('d-none');
   });
+  
 });
 
-$(document).ready(function() {
-  let spans = document.querySelectorAll('.word span');
-  spans.forEach((span, idx) => {
-    span.addEventListener('click', (event) => {
-      event.target.classList.add('active');
-    });
-    span.addEventListener('animationend', (event) => {
-      event.target.classList.remove('active');
-    });
-
-    setTimeout(() => {
-      span.classList.add('active');
-    }, 750 * (idx + 1));
-  });
-
-  $('.skillbar').each(function() {
-    $(this).find('.skillbar-bar').animate({
-      width: $(this).attr('data-percent')
-    }, 20000);
-  });
-
-  $('.hover').mouseleave(function() {
-    $(this).removeClass('hover');
-  });
-});
